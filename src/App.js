ee6f8db7bc0d7d1ec88f89f5f5762eef0d1c6aec
Matt Anderson <matthew.s.anderson@etrade.com>
2019-10-09 16:57:48 -0400

    Adding materialUI searchbar and gridlist:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import SearchBar from 'material-ui-search-bar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fetchedData: [
+        {img: 'https://supersimple.com/wp-content/uploads/hello-2-1080-740.jpg', title:'hello'},
+        {img: 'https://www.himgs.com/imagenes/hello/social/hello-fb-logo.png', title:'what'},
+        {img: 'https://i.ytimg.com/vi/ZJAbLpm3AXA/maxresdefault.jpg', title:'is'},
+        {img: 'https://cdn1.vectorstock.com/i/1000x1000/24/50/cartoon-lama-design-hello-card-with-cute-vector-20402450.jpg', title:'up my good man?'}
+      ]
+    };
+  }
+
+  render = () => {
+    return (
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <SearchBar
+            onChange={() => console.log('onChange')}
+            onRequestSearch={() => console.log('onRequestSearch')}
+            style={{
+              margin: '0 auto',
+              maxWidth: 800
+            }}
+          />
+          <GridList
+            cols={2}
+          >
+            // TODO - show results in grid;
+            {this.state.fetchedData.map(tile => (
+              <GridListTile key={tile.img} cols={tile.cols || 1}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
