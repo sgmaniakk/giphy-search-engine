4b8f02c4abcc7ba2c8e191a24b90741df0904e42
Matthew Anderson <sgmaniak@gmail.com>
2019-10-10 01:24:31 -0400

    beginning of pagination implimentation

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,23 @@ import GridListTile from '@material-ui/core/GridListTile';
 import SearchBar from 'material-ui-search-bar';
 import search from '../services/giphyAPI';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
+import ReactLoading from 'react-loading';
+import ReactPaginate from 'react-paginate';
+
+const PAGE_SIZE = 8;
+const INITIAL_PAGE = 0;
 
 class App extends React.Component {
+
   constructor() {
     super();
 
     this.state = {
       searchBarInput: '',
       fetchedData: [],
+      isLoading: false,
+      totalCount: 0,
+      currentPage: INITIAL_PAGE,
     };
   }
 
@@ -25,42 +34,85 @@ class App extends React.Component {
   }
 
   handleRequestSearch = async () => {
-    const data = await search(this.state.searchBarInput);
     this.setState({
       ...this.state,
-      fetchedData: data.data
+      isLoading: true,
+      currentPage: INITIAL_PAGE,
+    });
+    const data = await search(this.state.searchBarInput, PAGE_SIZE, INITIAL_PAGE);
+    this.setState({
+      ...this.state,
+      fetchedData: data.data,
+      isLoading: false,
+      totalCount: data.pagination.total_count,
+    });
+  }
+
+  handleChangePage = async (newPage) => {
+    this.setState({
+      ...this.state,
+      isLoading: true,
+      currentPage: newPage,
+    });
+    const data = await search(this.state.searchBarInput, PAGE_SIZE, newPage);
+    this.setState({
+      ...this.state,
+      fetchedData: data.data,
+      isLoading: false,
     });
   }
 
   render = () => {
+    console.log(Math.floor(this.state.totalCount/PAGE_SIZE), this.state.totalCount);
     return (
       <MuiThemeProvider>
         <div className="App">
           <h1>Gif Search</h1>
-          <SearchBar
-            onChange={(e) => this.handleInputChange(e)}
-            onRequestSearch={() => this.handleRequestSearch()}
-            style={{
-              marginBottom: '10%',
-              maxWidth: '50%',
-            }}
-          />
-          <GridList 
-            cols={2}
-            style={{
-              marginLeft: '10%',
-              marginRight: '10%',
-              marginBottom: '10%'
-            }}
-          >
-            {this.state.fetchedData.map(tile => (
-              <CopyToClipboard key={tile.id} text={tile.images.original.url}>
-                <GridListTile cols={tile.cols || 1}>
-                  <img src={tile.images.original.url} alt={tile.title}/>
-                </GridListTile>
-              </CopyToClipboard>
-            ))}
-          </GridList>
+          <div className="centered">
+            <SearchBar
+              onChange={(e) => this.handleInputChange(e)}
+              onRequestSearch={() => this.handleRequestSearch()}
+              style={{
+                marginBottom: '10%',
+                maxWidth: '50%',
+              }}
+            />
+          </div>
+          {this.state.isLoading ?
+            <ReactLoading className="centered" type={'spin'} color={'black'} height={'20%'} width={'20%'} />
+          :
+            (this.state.totalCount > 0 ?
+              <div>
+                <GridList
+                  className="centered"
+                  cols={2}
+                  style={{
+                    marginLeft: '10%',
+                    marginRight: '10%',
+                    marginBottom: '10%'
+                  }}
+                >
+                  {this.state.fetchedData.map(tile => (
+                    <CopyToClipboard key={tile.id} text={tile.images.original.url}>
+                      <GridListTile cols={tile.cols || 1}>
+                        <img src={tile.images.original.url} alt={tile.title}/>
+                      </GridListTile>
+                    </CopyToClipboard>
+                  ))}
+                </GridList>
+                <ReactPaginate
+                  previousLabel={'previous'}
+                  nextLabel={'next'}
+                  breakLabel={'...'}
+                  pageCount={Math.floor(this.state.totalCount/PAGE_SIZE)}
+                  pageRangeDisplayed={7}
+                  marginPagesDisplayed={3}
+                />
+              </div>
+              :
+              null
+            )
+          }
         </div>
       </MuiThemeProvider>
     );
--- src/services/giphyAPI.js
+++ src/services/giphyAPI.js
@@ -1,11 +1,10 @@
 const API_KEY = 'qnvnEumCMSDClNcGyAn5L3UX1igxrIkb';
 
-const search = async (query) => {
-  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query.replace(' ', '+')}&api_key=${API_KEY}&limit=8`, {
+const search = async (query, pageSize, currentPage) => {
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${query.replace(' ', '+')}&api_key=${API_KEY}&limit=${pageSize}&offset=${pageSize * currentPage}`, {
     method: 'GET',
   });
   const data = await response.json();
-  console.log(data);
   return data;
 };
 
